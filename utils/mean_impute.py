49eff694628fab4e546a8e01ebf5476a2329c693
codeanticode <andres.colubri@gmail.com>
2015-01-20 22:01:17 -0500

    added data to mean_impute

--- utils/mean_impute.py
+++ utils/mean_impute.py
@@ -6,8 +6,8 @@ It should not be used in practice! Just for dev purposes.
 import numpy as np
 import pandas as pd
 
-training_file = "training-data.csv"
-outfile = "training-data-imputed.csv"
+training_file = "./data/training-data.csv"
+outfile = "./data/training-data-imputed.csv"
 
 training = pd.DataFrame.from_csv(training_file)
 training.reset_index(inplace=True)
