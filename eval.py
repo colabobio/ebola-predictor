9c542ed01b3b10c8c5b7dd7ae8f39613349e4d72
codeanticode <andres.colubri@gmail.com>
2015-02-03 17:53:44 -0500

    output labels are stored in data/labels.txt

--- eval.py
+++ eval.py
@@ -1,6 +1,11 @@
 import argparse, glob, os
 from nnet.eval import eval as nnet_eval
 
+label_file = "./data/labels.txt"
+target_names = []
+with open(label_file, "rb") as vfile:
+    for line in vfile.readlines():
+        target_names.append(line.split()[1])
 predictors = ["nnet"]
 
 def avg_cal_dis():
@@ -14,6 +19,10 @@ def avg_report():
     for pred in predictors:
         print "Calculating average report for " + pred + "..."
         count = 0
+        total_prec = 0
+        total_rec = 0
+        total_f1 = 0
+        
         for testfile in test_files:
             start_idx = testfile.find("./data/testing-data-") + len("./data/testing-data-")
             stop_idx = testfile.find('.csv')
@@ -24,6 +33,23 @@ def avg_report():
                 count = count + 1
                 print id, testfile, pfile, trainfile
                 p, r, f, _ = nnet_eval(testfile, trainfile, pfile, 3)
+                total_prec += p
+                total_rec += r
+                total_f1 += f
+
+        avg_prec = (total_prec)/(count)
+        avg_rec = (total_rec)/(count)
+        avg_f1 = (total_f1)/(count)
+
+        output = '\n'
+        output += "Summary for NNET\n"
+        output += "{:10s} {:10s} {:10s} {:10s}".format("", "precision", "recall", "f1-score")+'\n'
+        output += "{:10s}      {:2.2f}    {:2.2f}        {:2.2f}".format(target_names[0], avg_prec[0], avg_rec[0], avg_f1[0])+'\n'
+        output += "{:10s}      {:2.2f}    {:2.2f}        {:2.2f}".format(target_names[1], avg_prec[1], avg_rec[1], avg_f1[1])+'\n'
+        output += "{:10s}      {:2.2f}    {:2.2f}        {:2.2f}".format("Total", (avg_prec[0] + avg_prec[1])/2, (avg_rec[0]+avg_rec[1])/2, (avg_f1[0]+avg_f1[1])/2)+'\n'
+        output += '\n'
+        print output
+
 
     print "report"
 
--- utils/classificationreport.py
+++ utils/classificationreport.py
@@ -1,16 +1,22 @@
-'''
+"""
 Builds a text report showing precision, recall, F1 score.
-'''
+"""
+
 from sklearn.metrics import classification_report
 from sklearn.metrics import precision_recall_fscore_support
 
+label_file = "./data/labels.txt"
+
 def report(probs, y_test):
+    preds = [int(0.5 < p) for p in probs]
 
-	preds = [int(0.5 < p) for p in probs]
+    target_names = []
+    with open(label_file, "rb") as vfile:
+        for line in vfile.readlines():
+            target_names.append(line.split()[1])
 
-	target_names = ['Survived', 'Died']
-	report = classification_report(y_test, preds, target_names=target_names)
+    report = classification_report(y_test, preds, target_names=target_names)
 
-	print report
-	
-	return precision_recall_fscore_support(y_test, preds)
+    print report
+
+    return precision_recall_fscore_support(y_test, preds)
