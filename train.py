eab017e1af4c3a0032305122b79141a8b53b2119
codeanticode <andres.colubri@gmail.com>
2015-02-05 18:02:07 -0500

    train script uses module loading

--- train.py
+++ train.py
@@ -2,33 +2,40 @@
 Trains all the predictors and saves the parameters to the data folder for later 
 evaluation.
 
-TODO: add module import to train other predictors
-
 @copyright: The Broad Institute of MIT and Harvard 2015
 """
 
-import os, argparse, glob
-from nnet.train import train as nnet_train
+import sys, os, argparse, glob
+from importlib import import_module
 
-if __name__ == "__main__":
+def train(predictor):
+    module_path = os.path.abspath(predictor)
+    module_filename = "train"
+    sys.path.insert(0, module_path)
+    module = import_module(module_filename)
+
     train_files = glob.glob("./data/training-data-completed-*.csv")
 
-    # remove old data
-    nnet_param_files = glob.glob("./data/nnet-params-*")
-    dtree_param_files = glob.glob("./data/dtree-params-*")
-    if nnet_param_files or dtree_param_files:
-        print "Removing old parameters..."
-        for file in nnet_param_files:
-            os.remove(file)
-        for file in dtree_param_files:
+    # remove old parameters
+    param_files = glob.glob("./data/" + module.prefix() + "-params-*")
+    if param_files:
+        print "Removing old " + module.title() + " parameters..."
+        for file in param_files:
             os.remove(file)
         print "Done."
 
-    print "Training all predictors..."
+    print "Training " + module.title() + " predictor..."
     for tfile in train_files:
         print "Training set: " + tfile + "..."
         start_idx = tfile.find("training-data-completed-") + len("training-data-completed-")
         stop_idx = tfile.find(".csv")
         id = tfile[start_idx:stop_idx]
-        nnet_train(train_filename=tfile, param_filename="./data/nnet-params-" + str(id))
+        module.train(train_filename=tfile, param_filename="./data/" + module.prefix() + "-params-" + str(id))
     print "Done."
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    # Evaluate the model with given method(s)
+    parser.add_argument('pred', nargs=1, default=["nnet"], help="Folder containing predictor to evaluate")
+    args = parser.parse_args()
+    train(args.pred[0])
