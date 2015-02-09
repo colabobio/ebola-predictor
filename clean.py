76ca83dde760c5edc13ce6b3da54bc57442e52fb
codeanticode <andres.colubri@gmail.com>
2015-02-09 15:42:20 -0500

    remove notes

--- clean.py
+++ clean.py
@@ -12,13 +12,12 @@ param_files = glob.glob("./data/*-params*")
 idx_files = glob.glob("./data/*-index*.csv")
 out_files = glob.glob("./out/*")
 
+print "Cleaning folders..."
+if os.path.exists("./data/notes.txt"): os.remove("./data/notes.txt")
 if test_files or train_files or param_files or idx_files or out_files:
-    print "Cleaning folders..."
     for file in test_files: os.remove(file)
     for file in train_files: os.remove(file)
     for file in param_files: os.remove(file)
     for file in idx_files: os.remove(file)
     for file in out_files: os.remove(file)
-    print "Done."
-else:
-    print "Nothing to do."
+print "Done."
