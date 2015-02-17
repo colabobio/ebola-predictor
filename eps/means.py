e2ae93dfb39f50ec9ffda8b3068848504e25fc48
codeanticode <andres.colubri@gmail.com>
2015-02-16 19:09:24 -0500

    fisher test

--- eps/means.py
+++ eps/means.py
@@ -6,36 +6,49 @@ Calculates ....
 
 import argparse, sys, os
 import numpy as np
+import pandas as pd
+from pandas.tools.pivot import pivot_table
 from scipy.stats import fisher_exact
 from scipy.stats import ttest_ind
 from utils import load_dataframe, design_matrix
 sys.path.append(os.path.abspath('./utils'))
 from evaluate import design_matrix as design_matrix_full
 
-def calculate(test_filename):
+def calculate(test_filename, def_thresh=None):
     df = load_dataframe(test_filename)
     X, y = design_matrix(df)
 
     surival_scores = df[df["OUT"] == 0]["SCORE"]
     death_scores = df[df["OUT"] == 1]["SCORE"]
     
+    # Test if the mean values of the scores among the recovered and deceased patients
+    # are significantly different:
     # http://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.ttest_ind.html
-    (tval, pval) = ttest_ind(surival_scores, death_scores, equal_var=False)
+    ttest_stat, ttest_pvalue = ttest_ind(surival_scores, death_scores, equal_var=False)
         
-    print np.mean(surival_scores),np.std(surival_scores)
-    print np.mean(death_scores),np.std(death_scores)
-    print "P-value:",pval
+    # Builds the 2x2 contingency table by defining the EPS prediction as:
+    # Survive: score <= mean(survival) + std(survival)
+    # Die: otherwise
+    if def_thresh: surv_thresh = def_thresh
+    else: surv_thresh = np.mean(surival_scores) + np.std(surival_scores) 
+    df["PRED"] = df["SCORE"].map(lambda x: 0 if x <= surv_thresh else 1)
+    print df    
+    df["VALUE"] = pd.Series(np.ones(df.shape[0]), index=df.index)
+    # http://pandas.pydata.org/pandas-docs/stable/generated/pandas.tools.pivot.pivot_table.html
+    counts = pivot_table(df, values="VALUE", index=["OUT"], columns=["PRED"], aggfunc=np.sum, fill_value=0)
+    # ...and performs a Fisher exact test on the 2x2 contingency table 
+    # http://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.fisher_exact.html
+    fisher_ratio, fisher_pvalue = fisher_exact(counts)
 
+    print "Mean,standard deviation of survival score:", np.mean(surival_scores),"",np.std(surival_scores)
+    print "Mean,standard deviation of death score   :", np.mean(death_scores),"",np.std(death_scores)
+    print "P-value of T-test to for means of survival and death scores :",ttest_pvalue    
+    print ""
     
-#     predictor = gen_predictor(cutoff)
-#     probs = predictor(X)
-#     indices = get_misses(probs, y)
-#     for i in indices:
-#         print "----------------"
-#         if meta: print "META:",",".join(lines[i].split(",")).strip()
-#         print "SCORE:", df.ix[i][1]
-#         print df0.ix[i]
-#     return indices
+    print "Observed outcome/EPS prediction contingency table"
+    print counts
+
+    print "P-Value of Fisher exact test on the Outcome/Prediction table:",fisher_pvalue
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
