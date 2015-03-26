58aeb0287a8b8f8c0d2ebb01d58afaa441db0935
codeanticode <andres.colubri@gmail.com>
2015-03-26 14:37:23 -0400

    removed non-ascii chars

--- randf/train.py
+++ randf/train.py
@@ -94,47 +94,7 @@ def train(train_filename, param_filename, **kwparams):
 #                   class_weight=class_weight, max_iter=max_iter, random_state=random_state)
 
 
-'''
-n_estimators : integer, optional (default=10)
-The number of trees in the forest.
 
-criterion : string, optional (default=”gini”)
-The function to measure the quality of a split. Supported criteria are “gini” for the Gini impurity and “entropy” for the information gain. Note: this parameter is tree-specific.
-
-max_features : int, float, string or None, optional (default=”auto”)
-The number of features to consider when looking for the best split:
-If int, then consider max_features features at each split.
-If float, then max_features is a percentage and int(max_features * n_features) features are considered at each split.
-If “auto”, then max_features=sqrt(n_features).
-If “sqrt”, then max_features=sqrt(n_features).
-If “log2”, then max_features=log2(n_features).
-If None, then max_features=n_features.
-Note: the search for a split does not stop until at least one valid partition of the node samples is found, even if it requires to effectively inspect more than max_features features. Note: this parameter is tree-specific.
-
-max_depth : integer or None, optional (default=None)
-The maximum depth of the tree. If None, then nodes are expanded until all leaves are pure or until all leaves contain less than min_samples_split samples. Ignored if max_samples_leaf is not None. Note: this parameter is tree-specific.
-
-min_samples_split : integer, optional (default=2)
-The minimum number of samples required to split an internal node. Note: this parameter is tree-specific.
-
-min_samples_leaf : integer, optional (default=1)
-The minimum number of samples in newly created leaves. A split is discarded if after the split, one of the leaves would contain less then min_samples_leaf samples. Note: this parameter is tree-specific.
-
-max_leaf_nodes : int or None, optional (default=None)
-Grow trees with max_leaf_nodes in best-first fashion. Best nodes are defined as relative reduction in impurity. If None then unlimited number of leaf nodes. If not None then max_depth will be ignored. Note: this parameter is tree-specific.
-
-bootstrap : boolean, optional (default=True)
-Whether bootstrap samples are used when building trees.
-
-oob_score : bool
-Whether to use out-of-bag samples to estimate the generalization error.
-
-n_jobs : integer, optional (default=1)
-The number of jobs to run in parallel for both fit and predict. If -1, then the number of jobs is set to the number of cores.
-
-random_state : int, RandomState instance or None, optional (default=None)
-If int, random_state is the seed used by the random number generator; If RandomState instance, random_state is the random number generator; If None, the random number generator is the RandomState instance used by np.random.
-'''
 
     clf = ensemble.RandomForestClassifier(criterion='entropy',min_samples_leaf=10, max_depth=5)
 
