efed1d26c6b3926061bdea371cbc54ceea578505
codeanticode <andres.colubri@gmail.com>
2015-04-06 12:23:50 -0400

    added arguments for job generation

--- gen_jobs.py
+++ gen_jobs.py
@@ -4,93 +4,36 @@ Exhaustive generation of models
 @copyright: The Broad Institute of MIT and Harvard 2015
 """
 
-import os, threading
-import time, glob, time
-import itertools
+import os, argparse, itertools
 
-total_sets = 10
-test_prec = 60
-model_sizes = [2]
-predictors = ["lreg", "scikit_lreg"]
-pred_options = {"lreg":"", 
-                "scikit_lreg":""}
-# predictors = ["lreg", "nnet", "scikit_lreg", "scikit_dtree", "scikit_svm", "scikit_randf"]
-# pred_options = {"lreg":"", 
-#                 "nnet":"", 
-#                 "scikit_lreg":"", 
-#                 "scikit_dtree":"min_samples_leaf=10 max_depth=5 criterion=entropy",
-#                 "scikit_svm":"error=10 kernel=rbf",
-#                 "scikit_randf":"min_samples_leaf=10 max_depth=5 criterion=entropy"}
-impute_method = "listdel"
-impute_fallback = "listdel"
-impute_options = {"listdel":"",
-                  "listdel":""}
 master_file = "./data/variables-master.txt"
-# var_file = "./data/variables.txt"
-max_restarts = 10
+model_sizes = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+clump_size = 6
 
-def get_last(name):
-    train_files = glob.glob("./models/" + name + "/training-data-completed*.csv")
-    idx = [int(fn[fn.rfind("-") + 1: fn.rfind(".")]) for fn in train_files]
-    idx.sort()
-    if idx: return idx[-1]
-    else: return -1
+def save_clump(count, vars):
+    print "Create file for job", count
+    with open("./jobs/job-" + str(count), "w") as jfile:
+        for k in vars:
+            jfile.write(str(k) + " " + ",".join(vars[k]) + "\n")
 
-def worker(name, count, first):
-    print "Start"
-#     os.system("python clean.py")
-    os.system("python init.py -N " + name + " -n " + str(count) + " -s " + str(first) + " -t " + str(test_prec) + " -m " + impute_method + " " + impute_options[impute_method])
-    return
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--model_sizes", nargs=1, default=["2,3,4"],
+                    help="list of model sizes")
+parser.add_argument("-c", "--clump_size", type=int, nargs=1, default=[5],
+                    help="size of models inside a single job")
+args = parser.parse_args()
 
-def create_var_file(mdl_count, fix_var, mdl_vars):
-    dir = "./models/" + str(mdl_count)
-    if not os.path.exists(dir):
-        os.makedirs(dir)
-    vfn = dir + "/variables.txt"
-    with open(vfn, "w") as vfile:
-        vfile.write(out_var + " " + var_dict[out_var] + "\n")
-        if fix_var: vfile.write(fix_var + " " + var_dict[fix_var] + "\n")
-        for v in mdl_vars:
-            vfile.write(v + " " + var_dict[v] + "\n")
+sizes = args.model_sizes[0]
+parts = sizes.split(',')
+model_sizes = []
+for s in parts:
+    if '-' in s:
+        t = s.split('-')
+        model_sizes.extend(range(int(t[0]), int(t[1]) + 1))
+    else:
+        model_sizes.append(int(s))
 
-def run_model(fix_var, mdl_vars, mdl_count):
-    vdict = [] 
-    if fix_var: vdict = [fix_var]
-    vdict.extend(mdl_vars)
-    name = str(mdl_count)
-    print "running model", name, vdict
-
-    count = total_sets
-    start = 0
-    nrest = 0
-    while True:
-        thread = threading.Thread(target=worker, args=(name, count, start))
-        thread.start()
-        while True:
-            time.sleep(0.1)
-            if not thread.isAlive():
-                break
-
-        n = get_last(name)
-        if n < total_sets - 1:
-            if max_restarts < nrest:
-                print "Model cannot be succesfully imputed, skipping!"
-                return 
-            start = n + 1
-            count = total_sets - start
-            nrest += 1
-        else:
-            print "Done! Number of restarts:", nrest
-            break
-            
-    for pred_name in predictors:
-        print "PREDICTOR",pred_name,"---------------"
-        pred_opt = pred_options[pred_name]
-        os.system("python train.py -N " + name + " " + pred_name + " "+ pred_opt)
-        repfn = "./models/" + name + "/report-" + pred_name + ".out"
-        os.system("python eval.py -N " + name + " -p " + pred_name + " -m report > " + repfn)
-
-##########################################################################################
+clump_size = args.clump_size[0]
 
 all_vars = []
 var_dict = {}
@@ -110,11 +53,26 @@ mdl_vars = all_vars[1:]
 if fix_var: mdl_vars.remove(fix_var)
 
 model_count = 0
+clump_count = 0
+clump_idx = 0
+clump_vars = {}
 for size in model_sizes:
     fsize = size
     if fix_var: fsize -= 1
     var_comb = itertools.combinations(mdl_vars, fsize)
     for mvars in var_comb:
-        create_var_file(model_count, fix_var, mvars)
-        run_model(fix_var, mvars, model_count)
+        if clump_idx < clump_size:
+            lvars = []
+            if fix_var: lvars.append(fix_var)
+            lvars.extend(mvars)
+            clump_vars[model_count] = lvars
+            clump_idx += 1
+        else:
+            save_clump(clump_count, clump_vars)
+            clump_count += 1
+            clump_idx = 0
+            clump_vars = {}
         model_count += 1
+
+if clump_vars:
+    save_clump(clump_count, clump_vars)
