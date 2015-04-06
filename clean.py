c40a2d0e41006f3dd023a212272eaf65a1d9c599
codeanticode <andres.colubri@gmail.com>
2015-04-06 11:53:20 -0400

    put back clean script, clean jobs

---
+++ clean.py
@@ -0,0 +1,40 @@
+"""
+Cleans folders
+
+@copyright: The Broad Institute of MIT and Harvard 2015
+"""
+
+import os, glob, argparse, shutil
+
+def delete(files):
+    for file in files:
+        if os.path.isdir(file):
+            shutil.rmtree(file)
+        else:
+            os.remove(file)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-m", "--clean_models", action="store_true",
+                    help="clean models folder")
+parser.add_argument("-j", "--clean_jobs", action="store_true",
+                    help="clean jobs folder")
+parser.add_argument("-o", "--clean_out", action="store_true",
+                        help="clean out folder")
+parser.add_argument("-a", "--clean_all", action="store_true",
+                    help="clean all folders")
+
+args = parser.parse_args()
+if args.clean_models or args.clean_all:
+    print "Cleaning models folder..."
+    delete(glob.glob("./models/*"))
+if args.clean_jobs or args.clean_all:
+    print "Cleaning jobs folder..."
+    delete(glob.glob("./jobs/*"))
+if args.clean_out or args.clean_all:
+    print "Cleaning out folder..."
+    delete(glob.glob("./out/*"))
+
+print "Cleaning temp folder..."
+delete(glob.glob("./temp/*"))
+
+print "Done."
