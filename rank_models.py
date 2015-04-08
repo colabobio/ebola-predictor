8ed40752e8458e30926288621813519322caeb55
codeanticode <andres.colubri@gmail.com>
2015-04-08 14:37:39 -0400

    some additional text

--- rank_models.py
+++ rank_models.py
@@ -76,6 +76,7 @@ for dir_name, subdir_list, file_list in os.walk(base_dir):
 
 print "Number of models:",mdl_count
 print "Number of incomplete models:",len(incomplete_models)
+
 sorted_preds = reversed(sorted(model_f1_scores.items(), key=operator.itemgetter(1)))
 with open(base_dir + "/ranking.txt", "w") as rfile:
     for pair in sorted_preds:
@@ -88,32 +89,10 @@ with open(base_dir + "/ranking.txt", "w") as rfile:
         mvars = model_vars[full_name]
         line = mdl_name + " " + pred_name + " " + ",".join(mvars) + " " + str(pred_f1_score) + " " + str(pred_f1_std)
         rfile.write(line + "\n")
+print "Saved ranking to",base_dir + "/ranking.txt"
 
 if incomplete_models:
     with open(base_dir + "/incomplete.txt", "w") as rfile:
         for mdl in incomplete_models:
             rfile.write(mdl + "\n")
-
-'''
-model_dirs = glob.glob(mdl_dir + "/*")
-for dir in model_dirs:
-    if not os.path.isdir(dir): continue
-    mdl_num = dir.split("/")[-1]
-    mdl_vars = load_vars(dir + "/variables.txt")
-    report_files = glob.glob(dir + "/report-*.out")
-    print "Reading model",mdl_num,"with variables", ",".join(mdl_vars)
-    for rfn in report_files:
-        with open(rfn, "r") as report:
-            pred = os.path.splitext(rfn.split("-")[-1])[0]
-            last = report.readlines()[-1]
-            parts = last.split(",")
-            mdl_name = mdl_num + "-" + pred
-            if 3 < len(parts):
-                model_f1_scores[mdl_name] = float(parts[3])
-                model_f1_dev[mdl_name] = float(parts[6])
-                model_vars[mdl_name] = mdl_vars
-            else:
-                print "  Cannot find scores, skipping!"
-
-
-'''
+    print "Saved incomplete models to",base_dir + "/incomplete.txt"
