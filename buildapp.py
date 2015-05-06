fe8ca53a8eae86e3494e51ded9419fce7b160ffe
codeanticode <andres.colubri@gmail.com>
2015-05-05 20:13:31 -0400

    add release option

--- buildapp.py
+++ buildapp.py
@@ -2,6 +2,8 @@
 
 import os
 
+#build_options = 'debug installd'
+build_options = 'release'
 curr_dir = os.getcwd()
 app_dir = os.path.join(curr_dir, 'evdcare')
 icon_file = os.path.join(app_dir, 'icon.png')
@@ -9,6 +11,8 @@ load_file = os.path.join(app_dir, 'loading.jpg')
 pfora_dir = os.path.abspath('../python-for-android/dist/default')
 os.chdir(pfora_dir)
 
-cmd_str = './build.py --package org.sabetilab.evdcare --name "EVD CARE" --version 0.1 --dir ' + app_dir + ' --orientation "sensor" --icon ' + icon_file + ' --presplash ' + load_file + ' debug installd'
+cmd_str = './build.py --package org.sabetilab.evdcare --name "EVD CARE" --version 0.1 --dir ' + app_dir + ' --orientation "sensor" --icon ' + icon_file + ' --presplash ' + load_file + ' ' + build_options
 os.system(cmd_str)
 os.chdir(curr_dir)
+
+
