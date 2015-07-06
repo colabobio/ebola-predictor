facb61ea21c42ada06b0a0a2a6d566bb2d576a00
codeanticode <andres.colubri@gmail.com>
2015-07-06 16:13:55 -0400

    change package name

--- buildapp.py
+++ buildapp.py
@@ -11,7 +11,7 @@ load_file = os.path.join(app_dir, 'images', 'loading.jpg')
 pfora_dir = os.path.abspath('../python-for-android/dist/default')
 os.chdir(pfora_dir)
 
-cmd_str = './build.py --package org.sabetilab.ebolacare --name "Ebola CARE" --version 0.5 --dir ' + app_dir + ' --orientation "sensor" --icon ' + icon_file + ' --presplash ' + load_file + ' ' + build_options
+cmd_str = './build.py --package org.broadinstitute.ebolacare --name "Ebola CARE" --version 0.5 --dir ' + app_dir + ' --orientation "sensor" --icon ' + icon_file + ' --presplash ' + load_file + ' ' + build_options
 os.system(cmd_str)
 os.chdir(curr_dir)
 
